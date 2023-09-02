88be35dab113191e04baeed082309863888d7da0
Nichollas Garcia <nichollascavalcanti@gmail.co.uk>
2023-09-02 12:41:03 +0100

    CSS needs to be improved, markups need to be add to editor

--- src/App.jsx
+++ src/App.jsx
@@ -3,32 +3,33 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
 
+function App() {
+  const [query,setQuery] = useState()
+
+  const handleChange = (e) =>{
+    return setQuery(e.target.value)
+  }
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='container'>
+
+        <h1>markdown previewer</h1>
+        <div className="editorBox">
+          <p>Editor</p>
+          <textarea
+          name="editortext"
+          id="editor"
+          cols="30"
+          rows="10"
+          onChange={handleChange}>
+          
+          </textarea>
+        </div>
+        <div className="queryBox">
+          <p>Query</p>
+          <div id='preview'>{query}</div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
   )
 }
 
